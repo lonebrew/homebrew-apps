e7ca72031f88470433949e6f4e9483e4521e8117
indianets <vijay@indianets.com>
2025-01-15 08:26:09 +0530

    boring.notch: change versioning scheme

--- Casks/boring.notch.rb
+++ Casks/boring.notch.rb
@@ -2,7 +2,7 @@ cask "boring.notch" do
   version "glowing-panda"
   sha256 "d893583bfc7e9d14c9bc14d82bca61aad5a77770ec28cccee1b276c97733115a"
 
-  url "https://github.com/TheBoredTeam/boring.notch/releases/download/#{version}/#{version.split("-").map(&:capitalize).join}.dmg"
+  url "https://github.com/TheBoredTeam/boring.notch/releases/download/#{version}/#{version.split(".").map(&:capitalize).join}.dmg"
   name "boring.notch"
   desc "Not so boring notch That Rocks"
   homepage "https://github.com/TheBoredTeam/boring.notch"
