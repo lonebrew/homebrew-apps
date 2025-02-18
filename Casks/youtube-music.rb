3bd5670da834c432776dcf1ec4278cdb54164db3
indianets <vijay@indianets.com>
2025-02-18 14:28:10 +0530

    youtube-music 3.7.4

--- Casks/youtube-music.rb
+++ Casks/youtube-music.rb
@@ -1,9 +1,9 @@
 cask "youtube-music" do
   arch arm: "-arm64", intel: ""
 
-  version "3.7.2"
-  sha256 arm:   "75ebd43cedd278cf50d752b4da086ccff9b4e732f19b2458da2fc75b4fe5db6d",
-         intel: "4ea510eea38e38683267abf4ebcd41fc063ff7e933474f0b1edc3fe995d3fa92"
+  version "3.7.4"
+  sha256 arm:   "926e793d2d7df57a3c2d66d593b60fb820a42f5954b0648fc7ff35223dfaf0e5",
+         intel: "db09b30957d08ec823c7e24081df583cd540558068efb184d1e688527b7dc207"
 
   url "https://github.com/th-ch/youtube-music/releases/download/v#{version}/YouTube-Music-#{version}#{arch}.dmg",
       verified: "github.com/th-ch/youtube-music/"
