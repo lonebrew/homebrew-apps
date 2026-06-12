75ed8933bf2dd265f4145f07478aa42fa6e57ca2
Vijay <vijay@indianets.com>
2026-06-13 00:29:32 +0530

    macytdl 1.30 (#27)

--- Casks/macytdl.rb
+++ Casks/macytdl.rb
@@ -1,11 +1,13 @@
 cask "macytdl" do
-  version "1.29.3"
-  sha256 "c77d680d0b185fd24e8eb2590d4e5f82996e01402dacadac47d5d98b8d7eece3"
+  version "1.30"
+  sha256 "17f7fdd48c0df10e144a3f98a1cf766c0142ea7061c7cce325234959bf27376a"
 
   url "https://github.com/section83/MacYTDL/releases/download/#{version}/MacYTDL-v#{version}.dmg"
   name "macytdl"
   desc "GUI front-end for the YT-DLP video downloader"
   homepage "https://github.com/section83/MacYTDL/"
+
+  depends_on :macos
 
   app "MacYTDL.app"
 
