4b4fde8cf2cbc7559bb89d25a9e71640c1284f02
indianets <vijay@indianets.com>
2026-09-21 16:33:09 +0530

    youtube-music: fix urls

--- Casks/youtube-music.rb
+++ Casks/youtube-music.rb
@@ -5,14 +5,14 @@ cask "youtube-music" do
   sha256 arm:   "a2d68bd80117ce376a08229f5cfb0a4570d98aba4e56af8040c725b22e856e0d",
          intel: "27f74b53c7c4dd8c77ce44d7c7d0f955cb083e5251701e6ca2b3781e85c4e373"
 
-  url "https://github.com/th-ch/youtube-music/releases/download/v#{version}/YouTube-Music-#{version}#{arch}.dmg"
+  url "https://github.com/pear-devs/pear-desktop/releases/download/v#{version}/YouTube-Music-#{version}#{arch}.dmg"
   name "YouTube Music"
   desc "Desktop App bundled with custom plugins (and built-in ad blocker / downloader)"
-  homepage "https://th-ch.github.io/youtube-music/"
+  homepage "https://github.com/pear-devs/pear-desktop/"
 
   auto_updates true
 
-  depends_on :macos
+  depends_on :monterey
 
   app "YouTube Music.app"
 
